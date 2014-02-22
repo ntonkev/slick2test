a587da7a068458d54b152dbc3e201ec14011ee9c
Alexandr Moroz <herrhound@gmail.com>
2014-02-21 22:55:54 -0800

    Generic CRUD implementation for Slick

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,9 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
+
+    //scala.slick.model.codegen.SourceCodeGenerator.main(Array("scala.slick.driver.PostgresDriver","org.postgresql.Driver","jdbc:postgresql://ec2-54-200-218-97.us-west-2.compute.amazonaws.com/sandbox?user=postgres&password=Password1?","c:\\Dev\\Projects\\slick2test\\modules\\datamodels\\app","DALTables"))
+
     Ok(views.html.index("Your new application is ready."))
   }
 
